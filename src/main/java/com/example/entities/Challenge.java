f869503d4a202f754f1421fb6417b4fa115b98c2
admin <c13731869@mydit.ie>
2017-02-20 11:52:01 +0000

    Challenge Tables showig usernames instead of ID

--- src/main/java/com/example/entities/Challenge.java
+++ src/main/java/com/example/entities/Challenge.java
@@ -17,15 +17,19 @@ public class Challenge {
     private int id;
 
     private int challengerId;
+    private String challengerName;
     private int opponentId;
+    private String opponentName;
     private boolean open;
 
     public Challenge() {
     }
 
-    public Challenge(int challengerId, int opponentId, boolean open) {
+    public Challenge(int challengerId, String challengerName, int opponentId, String opponentName, boolean open) {
         this.challengerId = challengerId;
+        this.challengerName = challengerName;
         this.opponentId = opponentId;
+        this.opponentName = opponentName;
         this.open = open;
     }
 
@@ -41,12 +45,28 @@ public class Challenge {
         this.challengerId = challengerId;
     }
 
+    public String getChallengerName() {
+        return challengerName;
+    }
+
+    public void setChallengerName(String challengerName) {
+        this.challengerName = challengerName;
+    }
+
     public int getOpponentId() {
         return opponentId;
     }
 
     public void setOpponentId(int opponentId) {
         this.opponentId = opponentId;
+    }
+
+    public String getOpponentName() {
+        return opponentName;
+    }
+
+    public void setOpponentName(String opponentName) {
+        this.opponentName = opponentName;
     }
 
     public boolean isOpen() {
@@ -62,7 +82,9 @@ public class Challenge {
         return "Challenge{" +
                 "id=" + id +
                 ", challengerId=" + challengerId +
+                ", challengerName='" + challengerName + '\'' +
                 ", opponentId=" + opponentId +
+                ", opponentName='" + opponentName + '\'' +
                 ", open=" + open +
                 '}';
     }
