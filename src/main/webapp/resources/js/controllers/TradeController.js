9d1e878528ca705219251cbdf628ea26eb2b8fb6
admin <c13731869@mydit.ie>
2017-02-28 16:10:24 +0000

    Checked trades operating correctly with live data

--- src/main/webapp/resources/js/controllers/TradeController.js
+++ src/main/webapp/resources/js/controllers/TradeController.js
@@ -1,21 +1,14 @@
 angular.module('myApp.TradeController',[]).
 controller('TradeController',function($scope,$http,$state,$cookieStore,$interval,$mdSidenav){
+
     $scope.currUser = $cookieStore.get('userCookie');
     $scope.stakes = ["100", "250","500","1000","2500","5000","10000"];
     $scope.available = $scope.currUser.account.balance;
-    $scope.leverage = 1000;
+    $scope.leverage = 300;
     $scope.mMargin=0;
 
-    $scope.openBuyRate=0;
-    $scope.openSellRate=0;
 
 
-
-    /**
-     * init is called initially to fill the rows with currency data
-     * then it is called again every four seconds to update
-     * THIS SECTION WORKS 1000%!!!
-     */
     $scope.init = function(){
         $http.get('/api/fight/pairs')
             .success(function (data, status) {
@@ -24,18 +17,17 @@ controller('TradeController',function($scope,$http,$state,$cookieStore,$interval
                 }
             }).error(function (error) {
             console.log("something went wrong in the pairs controller init function!!");
-        });//end http.get
-    }//end function
+        });
+    }
+    $scope.init();
     $interval( function(){ $scope.init(); }, 4000);
 
 
-    /**
-     *
-     * SideNav Section
-     */
+
+
+
     $scope.toggleLeft = buildToggler('left');
     $scope.toggleRight = buildToggler('right');
-
     function buildToggler(componentId) {
         return function(x,y) {
 
@@ -46,7 +38,6 @@ controller('TradeController',function($scope,$http,$state,$cookieStore,$interval
             $mdSidenav(componentId).toggle();
         };
     }
-
 
     $scope.closeToggleLeft = buildTogglerClose('left');
     function buildTogglerClose(componentId) {
@@ -67,7 +58,6 @@ controller('TradeController',function($scope,$http,$state,$cookieStore,$interval
         tradeObject.pairSymbols = $scope.pairChosen.symbols;
         tradeObject.stake = $scope.currUserStake;
         tradeObject.action = $scope.direction;
-        // console.log(tradeObject.playerID, tradeObject.pairID,tradeObject.stake,tradeObject.action);
 
 
 
@@ -78,25 +68,12 @@ controller('TradeController',function($scope,$http,$state,$cookieStore,$interval
                 }
             }).error(function (error) {
             console.log("something went wrong in pairs call inside watch markets!!");
-        });//end http.get
+        });
 
-        /**
-         * reduce amount available each time trader opens a position
-         */
+
         $scope.available = $scope.available - $scope.currUserStake;
 
-
-        /**
-         * Maintanance Margin is always half the original margin
-         * Account equiry must exceed the mMargin level, or position is automatically closed
-         * increased every time a trader opens a position
-         */
         $scope.mMargin += (($scope.currUserStake / 2));
-
-
-        /**
-         * THIS SECTION, WORKS 100% for Direct Quotes (USD the counter currency)
-         */
 
         $scope.openBuyRate = $scope.pairChosen.ask;
         $scope.openSellRate = $scope.pairChosen.bid;
@@ -113,9 +90,9 @@ controller('TradeController',function($scope,$http,$state,$cookieStore,$interval
         }else{
             $scope.watch("cross")
         }
+    }
 
 
-    }//end trade()
 
     $scope.closeTrade = function(x){
 
@@ -128,10 +105,8 @@ controller('TradeController',function($scope,$http,$state,$cookieStore,$interval
 
 
 
-
     $scope.watch = function(param){
         $scope.watchMarkets = function(){
-            console.log("watching markets...");
 
             $http.post('/api/fight/getThisPair',$scope.pairChosenSym)
                 .success(function (data, status) {
@@ -140,19 +115,17 @@ controller('TradeController',function($scope,$http,$state,$cookieStore,$interval
                     }
                 }).error(function (error) {
                 console.log("something went wrong in pairs call inside watch markets!!");
-            });//end http.get
+            });
 
-            if(param.match("/USD")){
-                $scope.directQuoteCalc($scope.pairChosen,$scope.direction);
-            }else if(param.match("USD/")){
-                $scope.indirectQuoteCalc($scope.pairChosen,$scope.direction);
-            }else if(param.match("cross")){
-                $scope.crossQuoteCalc($scope.pairChosen,$scope.direction);
-            }
+            $scope.calculatePositions(param);
 
         }
         $interval( function(){ $scope.watchMarkets(); }, 4000);
+        $scope.calculatePositions(param);
+    }
 
+
+    $scope.calculatePositions = function(param){
         if(param.match("/USD")){
             $scope.directQuoteCalc($scope.pairChosen,$scope.direction);
         }else if(param.match("USD/")){
@@ -164,24 +137,10 @@ controller('TradeController',function($scope,$http,$state,$cookieStore,$interval
 
 
 
-
-
-
-
-
-    /**
-     * Calculation Section
-     * @param pair
-     * @param direction
-     */
-
     $scope.directQuoteCalc = function(pair,direction){
 
-        $scope.currAsk = pair.ask;
-        $scope.currBid = pair.bid;
-
-        $scope.closeSellRate = $scope.currBid;
-        $scope.closeAskRate = $scope.currAsk;
+        $scope.closeSellRate = pair.bid;
+        $scope.closeAskRate = pair.ask;
 
         $scope.longPipDifference = $scope.closeSellRate - $scope.openBuyRate;
         $scope.shortPipDifference = $scope.openSellRate - $scope.closeAskRate;
@@ -200,20 +159,13 @@ controller('TradeController',function($scope,$http,$state,$cookieStore,$interval
             $scope.profitAndLossView = $scope.profitAndLoss.toFixed(2);
             $scope.equity = ($scope.available + $scope.profitAndLoss + parseFloat($scope.currUserStake));
             $scope.equityView = $scope.equity.toFixed(2);
-
-
         }
     }
 
     $scope.indirectQuoteCalc = function(pair,direction){
-        /**
-         * curr ask and curr bid are used for calculating the P&L, whether (buy or sell)
-         */
-        $scope.currAsk = pair.ask;
-        $scope.currBid = pair.bid;
 
-        $scope.closeSellRate = $scope.currBid;
-        $scope.closeAskRate = $scope.currAsk;
+        $scope.closeSellRate = pair.bid;
+        $scope.closeAskRate = pair.ask;
 
 
         $scope.longPipDifference = $scope.closeSellRate - $scope.openBuyRate;
@@ -272,8 +224,8 @@ controller('TradeController',function($scope,$http,$state,$cookieStore,$interval
                 $scope.equity = ($scope.available + $scope.profitAndLoss + parseFloat($scope.currUserStake));
                 $scope.equityView = $scope.equity.toFixed(2);
             }
-        }//end else
-    }//end crossQuoteCalc
+        }
+    }
 
 $scope.getBase = function(){
     $http.post('/api/fight/getThisPair',$scope.param)
@@ -282,13 +234,12 @@ $scope.getBase = function(){
 
                 $scope.baseAsk = data.ask;
                 $scope.baseBid = data.bid;
-
                 $scope.setCrossProfit();
             }
         }).error(function (error) {
-        console.log("something went wrong in getBase() -> getThisPair!!");
-    });//end http.get
+    });
 }
+
 
 });
 
