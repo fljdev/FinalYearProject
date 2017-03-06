670df07c4c7c2875abe4266e1c3ab7fffefe1c1d
admin <c13731869@mydit.ie>
2017-03-06 22:41:57 +0000

    Rank (sorting based on account balances {backend only, but working 100%}}

--- src/main/java/com/example/controllers/RankRestController.java
+++ src/main/java/com/example/controllers/RankRestController.java
@@ -1,14 +1,15 @@
 package com.example.controllers;
 
 import com.example.entities.Rank;
+import com.example.entities.User;
 import com.example.services.IRankService;
+import com.example.services.IUserService;
+import com.sun.tools.javac.util.ArrayUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Created by admin on 02/03/2017.
@@ -20,6 +21,13 @@ import java.util.ArrayList;
 public class RankRestController {
 
     IRankService iRankService;
+    IUserService iUserService;
+
+    @Autowired
+    public void setiUserService(IUserService iUserService) {
+
+        this.iUserService = iUserService;
+    }
 
     @Autowired
     public void setiRankService(IRankService service) {
@@ -33,6 +41,42 @@ public class RankRestController {
         ranks = (ArrayList<Rank>) iRankService.getALlRanks();
         return ranks;
     }
+
+    @RequestMapping(value = "/saveRank", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public ArrayList<User> saveRank() {
+
+        ArrayList<User> users = iUserService.getAllUsers();
+        ArrayList<User> rankedByBalanceList = new ArrayList<>();
+        ArrayList<Double> balances = new ArrayList<>();
+
+        for(User u : users){
+            balances.add(u.getAccount().getBalance());
+        }
+        Collections.sort(balances);
+
+        for(int i = balances.size()-1; i>=0 ; i--){
+            for(User u : users){
+                if(u.getAccount().getBalance()==balances.get(i)){
+                    rankedByBalanceList.add(u);
+                }
+            }
+        }
+
+        for(User u : rankedByBalanceList){
+            System.out.println("user in ranked list is "+u.getUsername()+" £"+u.getAccount().getBalance());
+        }
+
+
+        Rank rank = new Rank();
+
+
+
+
+
+        return users;
+    }
+
 
 
 
--- src/main/webapp/resources/js/controllers/RankController.js
+++ src/main/webapp/resources/js/controllers/RankController.js
@@ -5,27 +5,37 @@ controller('RankController',function($scope, $stateParams, $cookieStore, $http){
 
     $scope.currUser = $cookieStore.get('userCookie').username;
 
-    if($scope.currUser){
-        $http.post('/api/user/findById', JSON.stringify($scope.currUser.id))
-            .success(function (data, status) {
-                if(status = 200){
-                    $cookieStore.put('userCookie', data);
-                }
-            }).error(function (error) {
-            console.log("something went wrong in findById -> RankController!!");
-        });
-    }
+    // if($scope.currUser){
+    //     $http.post('/api/user/findById', JSON.stringify($scope.currUser.id))
+    //         .success(function (data, status) {
+    //             if(status = 200){
+    //                 $cookieStore.put('userCookie', data);
+    //             }
+    //         }).error(function (error) {
+    //         console.log("something went wrong in findById -> RankController!!");
+    //     });
+    // }
 
 
 
     $scope.init = function(){
-        $http.get('/api/user/allRanks')
+        // $http.get('/api/rank/allRanks')
+        //     .success(function (data, status) {
+        //         if(status = 200){
+        //             $scope.allRanks = data;
+        //         }
+        //     }).error(function (error) {
+        //     console.log("something went wrong getting allRanks!!");
+        // });
+
+        $http.get('/api/rank/saveRank')
             .success(function (data, status) {
                 if(status = 200){
-                    $scope.allRanks = data;
+                    // $scope.allRanks = data;\
+                    console.log(",saveRank returned : ",data);
                 }
             }).error(function (error) {
-            console.log("something went wrong getting allRanks!!");
+            console.log("something went wrong getting saveRanks!!");
         });
     }
     $scope.init();
