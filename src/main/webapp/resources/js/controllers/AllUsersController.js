065c6ae44b075c8c4fe986a4bc95513b57466fe9
admin <c13731869@mydit.ie>
2017-03-10 12:58:19 +0000

    Removed balance from allUsers showed rank instead also removed "online" column

--- src/main/webapp/resources/js/controllers/AllUsersController.js
+++ src/main/webapp/resources/js/controllers/AllUsersController.js
@@ -5,13 +5,12 @@ controller('AllUsersController', function($scope,$http){
             $http.get('/api/user/allUsers')
                 .success(function (data, status) {
                     if(status = 200){
-                        //data will be equal to the arraylist returned by the UserRestController
                         $scope.allUsers = data;
                     }
                 }).error(function (error) {
                 console.log("something went wrong!!");
-            });//end http.get
-    }//end function
+            });
+    };
     $scope.init();
 
-});//end controller
+});
